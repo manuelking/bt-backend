7087c3bc83ea76cd6051d98eebe95c4b8acc778a
manuelking <emmanuel_dunyo@outlook.com>
2025-01-03 20:04:39 +0000

    allowing all origins

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,19 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   async headers() {
-    const allowedOrigin = process.env.NEXT_PUBLIC_ALLOWED_ORIGIN
+    // const allowedOrigin = process.env.NEXT_PUBLIC_ALLOWED_ORIGIN
 
-    if (!allowedOrigin) {
-      console.error('Allowed origin is not set in the environment variable')
-      return []
-    }
+    // if (!allowedOrigin) {
+    //   console.error('Allowed origin is not set in the environment variable')
+    //   return []
+    // }
     return [
       {
         source: '/api/:path*',
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: allowedOrigin,
+            value: '*', //allowedOrigin,
           },
           {
             key: 'Access-Control-Allow-Methods',
